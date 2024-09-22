06173c8723abb72c89a41d632f25d5795f0eb7cd
Anastasia Dubrovina <lukinova.anastasiya@mail.ru>
2024-09-22 15:27:20 +0300

    123

--- src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -1,35 +1,77 @@
 package jm.task.core.jdbc.dao;
 
 import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.util.Util;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.List;
 
 public class UserDaoJDBCImpl implements UserDao {
-    public UserDaoJDBCImpl() {
 
+    public UserDaoJDBCImpl() {
     }
 
     public void createUsersTable() {
-
+        try (Statement statement = Util.getConnection().createStatement()) {
+            statement.execute("CREATE TABLE if not exists users (id bigint AUTO_INCREMENT PRIMARY KEY,name varchar (60),lastName varchar (60), age int);");
+        } catch (SQLException e) {
+            e.getStackTrace();
+        }
     }
 
     public void dropUsersTable() {
-
+        try (Statement statement = Util.getConnection().createStatement()) {
+            statement.execute("DROP TABLE if exists users");
+        } catch (SQLException e) {
+            e.getStackTrace();
+        }
     }
 
     public void saveUser(String name, String lastName, byte age) {
-
+        try (Statement statement = Util.getConnection().createStatement()) {
+            statement.executeUpdate(
+                    "insert  into  users(name, lastName, age) " +
+                            "values ('" + name + "', '" + lastName + "', " + age + ")");
+            System.out.println("User с именем — " + name + " добавлен в базу данных");
+        } catch (SQLException e) {
+            e.getStackTrace();
+        }
     }
 
     public void removeUserById(long id) {
-
+        try (Statement statement = Util.getConnection().createStatement()) {
+            statement.execute("DELETE from users WHERE id IN(" + id + ")");
+        } catch (SQLException e) {
+            e.getStackTrace();
+        }
     }
 
     public List<User> getAllUsers() {
-        return null;
+        List<User> users = new ArrayList<>();
+        try (Statement statement = Util.getConnection().createStatement()) {
+            ResultSet results = statement.executeQuery("SELECT*FROM users");
+            while (results.next()) {
+                User user = new User();
+                user.setId(results.getLong("id"));
+                user.setName(results.getString("name"));
+                user.setLastName(results.getString("lastName"));
+                user.setAge(results.getByte("age"));
+                users.add(user);
+            }
+        } catch (SQLException e) {
+            e.getStackTrace();
+        }
+        return users;
     }
 
     public void cleanUsersTable() {
-
+        try (Statement statement = Util.getConnection().createStatement()) {
+            statement.execute("TRUNCATE users");
+        } catch (SQLException e) {
+            e.getStackTrace();
+        }
     }
 }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,23 @@
 package jm.task.core.jdbc.util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Util {
     // реализуйте настройку соеденения с БД
+    private final static String URL = "jdbc:mysql://localhost:3306/sod";
+    private final static String USER = "root";
+    private final static String PASSWORD = "sodluc1213";
+
+    public static Connection getConnection() {
+        Connection connection;
+        try {
+            connection = DriverManager.getConnection(URL, USER, PASSWORD);
+            return connection;
+        } catch (SQLException e) {
+            e.getStackTrace();
+            return null;
+        }
+    }
 }
