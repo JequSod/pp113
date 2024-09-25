795b601492eb874571691d7321cd182b51a46ab5
Evgeny Dubrovin <lukinova.anastasiya@mail.ru>
2024-09-25 15:00:53 +0300

    134

--- src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
@@ -1,42 +1,82 @@
 package jm.task.core.jdbc.dao;
 
 import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.util.Util;
+import org.hibernate.Session;
+import org.hibernate.query.Query;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class UserDaoHibernateImpl implements UserDao {
     public UserDaoHibernateImpl() {
-
     }
-
 
     @Override
     public void createUsersTable() {
-
+        try (Session session = Util.getSessionFactory().openSession()) {
+            session.beginTransaction();
+            Query query = session.createSQLQuery("CREATE TABLE if not exists users (id bigint AUTO_INCREMENT PRIMARY KEY,name varchar (60),lastName varchar (60), age int);");
+            query.executeUpdate();
+        } catch (Exception e) {
+            e.getStackTrace();
+        }
     }
 
     @Override
     public void dropUsersTable() {
-
+        try (Session session = Util.getSessionFactory().openSession()) {
+            session.beginTransaction();
+            Query query = session.createSQLQuery("DROP TABLE IF EXISTS users").addEntity(User.class);
+            query.executeUpdate();
+            session.getTransaction().commit();
+        } catch (Exception e) {
+            e.getStackTrace();
+        }
     }
 
     @Override
     public void saveUser(String name, String lastName, byte age) {
-
+        try (Session session = Util.getSessionFactory().openSession()) {
+            session.beginTransaction();
+            session.save(new User(name, lastName, age));
+            session.getTransaction().commit();
+        } catch (Exception e) {
+            e.getStackTrace();
+        }
     }
 
     @Override
     public void removeUserById(long id) {
-
+        try (Session session = Util.getSessionFactory().openSession()) {
+            session.beginTransaction();
+            Query query = session.createSQLQuery("DELETE from users WHERE id IN(" + id + ")");
+            query.executeUpdate();
+        } catch (Exception e) {
+            e.getStackTrace();
+        }
     }
 
     @Override
     public List<User> getAllUsers() {
-        return null;
+        List<User> list = new ArrayList<>();
+        try (Session session = Util.getSessionFactory().openSession()) {
+            list = session.createQuery("from User ", User.class).list();
+        } catch (Exception e) {
+            e.getStackTrace();
+        }
+        return list;
     }
 
     @Override
     public void cleanUsersTable() {
-
+        try (Session session = Util.getSessionFactory().openSession()) {
+            session.beginTransaction();
+            Query query = session.createSQLQuery("TRUNCATE TABLE Users").addEntity(User.class);
+            query.executeUpdate();
+            session.getTransaction().commit();
+        } catch (Exception e) {
+            e.getStackTrace();
+        }
     }
 }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,10 @@
 package jm.task.core.jdbc.util;
 
+import jm.task.core.jdbc.model.User;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
@@ -20,4 +25,12 @@ public class Util {
             return null;
         }
     }
+
+    public static SessionFactory getSessionFactory() {
+        Configuration configuration = new Configuration().addAnnotatedClass(User.class);
+        return configuration.buildSessionFactory();
+    }
+
+
+
 }
